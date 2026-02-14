cd026bc37144efa8b66fc81cfb8d7f8230f0342c
ThanapatrP <thanapatr.pul@gmail.com>
2026-02-14 18:12:59 +0700

    adjust product card

--- src/comps/product-card/ProductCard.jsx
+++ src/comps/product-card/ProductCard.jsx
@@ -8,16 +8,14 @@ function ProductCard(props) {
   const productName = props.hasOwnProperty("name") ? props.name : "Not found";
 
   return (
-    <div className="border-2 border-solid rounded-md m-2 p-4 w-70 hover:cursor-pointer hover:text-black hover:bg-white">
+    <div className="border-2 border-solid rounded-md m-2 p-4 h-90 w-70 hover:cursor-pointer hover:text-black hover:bg-white">
+      <h2 className="m-auto text-center courier-prime-bold">{productName}</h2>
       <img
-        className="rounded-md m-auto h-50 w-60 object-cover hover:border-black border-2"
+        className="rounded-md m-auto h-50 w-60 object-cover hover:border-black border-2 my-2"
         src={imageUrl}
         alt=""
       />
-      <h2 className="m-auto text-center courier-prime-bold mt-2">
-        {productName}
-      </h2>
-      <p className="m-auto courier-prime-regular">
+      <p className="m-auto line-clamp-4 courier-prime-regular">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae dolore
         molestiae modi? Velit deleniti laborum suscipit? Atque magnam adipisci
         placeat culpa cupiditate corrupti possimus rem, aperiam nesciunt
