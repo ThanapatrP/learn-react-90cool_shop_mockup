f56856389b4cbb6626e30bef90ef6732491618c3
ThanapatrP <thanapatr.pul@gmail.com>
2026-02-14 17:31:09 +0700

    connect tailwindcss with react app & apply tailwind for ProductCard component

--- src/comps/product-card/ProductCard.jsx
+++ src/comps/product-card/ProductCard.jsx
@@ -3,15 +3,26 @@ function ProductCard(props) {
 
   const imageUrl = props.hasOwnProperty("imageUrl")
     ? props.imageUrl
-    : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6OFdYEiUBHCvCWNHWq5OeMX0nh7P5sdviSg&s";
+    : "https://jobs.ficsi.in/assets/front_end/images/no-image-found.jpg";
 
-  const productName = props.hasOwnProperty("name") ? props.name : "N/A";
+  const productName = props.hasOwnProperty("name") ? props.name : "Not found";
 
   return (
-    <div className="border-2 border-solid">
-      <img className="productImage" src={imageUrl} alt="" />
-      <h2 className="productHeader">{productName}</h2>
-      <p className="productDetail">TestTestTestTestTestTestTestTestTestTest</p>
+    <div className="border-2 border-solid rounded-md m-2 p-4 w-70 hover:cursor-pointer hover:text-black hover:bg-white">
+      <img
+        className="rounded-md m-auto h-50 w-60 object-cover hover:border-black border-2"
+        src={imageUrl}
+        alt=""
+      />
+      <h2 className="m-auto text-center courier-prime-bold mt-2">
+        {productName}
+      </h2>
+      <p className="m-auto courier-prime-regular">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae dolore
+        molestiae modi? Velit deleniti laborum suscipit? Atque magnam adipisci
+        placeat culpa cupiditate corrupti possimus rem, aperiam nesciunt
+        explicabo vero esse?
+      </p>
     </div>
   );
 }
